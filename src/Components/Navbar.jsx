43d179deaeaf2a3dd60e9c2ab2423a8fa267828d
dev-ankit24 <ankitkumar839282@gmail.com>
2024-07-11 16:36:22 +0530

    fix bug navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 export default function Navbar() {
   return (
     <>
-    <nav className='flex'>
+    <nav className='flex  flex-col lg:flex-row justify-between py-3 mx-6'>
       <div>
-        <h3>{new Date().toUTCString().slice(0,16)}</h3>
-        <h1>Flavoro Foods</h1>
+        <h3 className='text-xl text-gray-600 font-bold'> { new Date().toUTCString().slice(0,16)}</h3>
+        <h1 className='text-3xl font-bold py-2 '>Flavoro Foods</h1>
       </div>
       <div>
-        <input type="search" name='search' placeholder='Search Here Food' />
+        <input type="search" name='search' placeholder='Search Here Food' className='border border-gray-300  rounded-lg outline-none py-3 my-2 w-full lg:w-[25vw]' />
       </div>
     </nav>
     </>
