ce0c8e3ba5f9e1ddd49b6ab969f218a785677de7
dev-ankit24 <ankitkumar839282@gmail.com>
2024-07-14 16:24:12 +0530

    Create Menu item

--- src/Components/CategoryMenu.jsx
+++ src/Components/CategoryMenu.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import FoodData from '../Data/FoodData';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCategory } from '../Redux/Slice/CategorySlice';
+
 
 export default function CategoryMenu() {
   let [categories, setCategories]= useState([]);
+  let dispatch =useDispatch()
+  let selectedCategory =useSelector((state)=>state.category.category)
+
   const listCategories=()=>{
   let findCategories= [...new Set(FoodData.map((food)=>food.category))];
   setCategories(findCategories)
-  console.log(categories);
   }
   useEffect(()=>{
     listCategories()
@@ -18,13 +23,20 @@ export default function CategoryMenu() {
         <h3 className='text-2xl font-semibold font-serif  '>Find the Best Food</h3>
     </div>
     <div className='flex gap-2 lg:gap-3  ' >
-        <button className='text-xl  font-semibold py-2 px-3 bg-gray-300 hover:bg-green-600 hover:text-white  my-4 rounded-lg'>
-          All</button>
-       {categories.map((item,index)=>{
-        return(<button key={index} className='text-xl  font-semibold py-2 px-3 bg-gray-300 hover:bg-green-600 hover:text-white  my-4 rounded-lg'>
+      {console.log(selectedCategory)}
+        <button
+        onClick={()=>dispatch(setCategory("All"))}
+        className={`text-xl  font-semibold py-2 px-3 bg-gray-300 hover:bg-green-600 hover:text-white  my-4 rounded-lg ${ selectedCategory === "All" && "bg-green-600 text-white"}` }>All</button>
+       {
+       categories.map((item,index)=>{
+        return(<button key={index} 
+          // select item onClick function
+          onClick={()=>dispatch(setCategory(item))}
+        className={`text-xl  font-semibold py-2 px-3 bg-gray-300 hover:bg-green-600 hover:text-white  my-4 rounded-lg ${selectedCategory===item && "bg-green-600 text-white"}`}>
           {item}</button>
         )
-       })}
+       })
+       }
     </div>
     </div>
     </>
--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit"
 import CartSlice from "./Slice/CartSlice";
+import CategorySlice from "./Slice/CategorySlice";
 
 const Store = configureStore({
     reducer:{
         cart:CartSlice,
+        category:CategorySlice,
     }
 });
 export default Store;
